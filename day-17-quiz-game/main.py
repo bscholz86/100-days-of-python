769dafbf57d5386895c00ac85a23458053480787
Ben Scholz <ben.scholz86@gmail.com>
2025-06-10 17:00:48 +0930

    Start of Day 17 Quiz Game Tutorial.

---
+++ day-17-quiz-game/main.py
@@ -0,0 +1,13 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for q in question_data:
+    the_question = Question(q["text"],q["answer"])
+    question_bank.append(the_question)
+
+quiz_brain = QuizBrain(question_bank)
+while quiz_brain.still_has_questions():
+    quiz_brain.next_question()
