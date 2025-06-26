479707de1531d125fb814efd9e7014619ad720d3
Ben Scholz <ben.scholz86@gmail.com>
2025-06-26 18:52:51 +0930

    Start Pandas project.

---
+++ day-25-weather-spreadsheet-exercise/main.py
@@ -0,0 +1,43 @@
+# data = []
+# with open("weather_data.csv") as weather_data:
+#     for data_set in weather_data.readlines():
+#         data_set = data_set.strip()
+#         data.append(data_set)
+#
+# print(data)
+
+# import csv
+# with open("weather_data.csv") as weather_data:
+#     data = csv.reader(weather_data)
+#     temperatures = []
+#
+#     for row in data:
+#         if row[1] != "temp": # Skip column heading "temp" in the .csv file.
+#             temp = row[1]
+#             temperatures.append(int(temp))
+#
+# print(temperatures)
+
+import pandas
+# https://pandas.pydata.org
+
+data = pandas.read_csv(filepath_or_buffer="weather_data.csv")
+# print(data["temp"])
+# print(data)
+data_dict = data.to_dict()
+print(data_dict)
+temp_list = data["temp"].to_list()
+print(temp_list)
+# average_temp = sum(temp_list) / len(temp_list)
+# print(f"{average_temp:.2f}")
+
+print(data["temp"].mean())
+print(data["temp"].max())
+
+# Get Data in columns
+print(data.temp) # is the same as:
+print(data["temp"])
+
+# Get Data in rows
+print(data[data.day == "Monday"])
+print(data[data.temp == data.temp.max()]) # Gets row where temp was maximum.
