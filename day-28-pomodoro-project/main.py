2344029ca56179f172104cae1ead0da47841fc0e
Ben Scholz <ben.scholz86@gmail.com>
2025-07-01 16:42:15 +0930

    Finished Day 28 Pomodoro Project. Recreated bug where timer could be started multiple times and then fixed it again by creating an additional function (button_start_pressed()) to detect if the start button has already been pressed.
    Added console print statements to help follow program flow.

--- day-28-pomodoro-project/main.py
+++ day-28-pomodoro-project/main.py
@@ -6,37 +6,86 @@ RED = "#e7305b"
 GREEN = "#379b46"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = float(25)
+SHORT_BREAK_MIN = float(5)
+LONG_BREAK_MIN = float(20)
 
-start_timing = False
+start_pressed = False
+timer = None
 reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    global start_timing
-    start_timing = False
-    canvas.itemconfig(timer_text, text=f"{WORK_MIN}:00")
+    try:
+        global reps
+        global start_pressed
+        window.after_cancel(timer)
+        reps = 0
+        canvas.itemconfig(timer_text, text="00:00")
+        main_title.config(text="Timer",fg=GREEN)
+        checkmarks.config(text="")
+        start_pressed = False
+    except ValueError:
+        print("Timer wasn't initiated")
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
+def button_start_pressed():
+    """Function used to stop the start button from starting the timer multiple times."""
+    print("Start button pressed")
+    global start_pressed
+
+    if not start_pressed:
+        start_timer()
+        start_pressed = True
+    else:
+        print("Timer already started")
+
 def start_timer():
     global reps
-    global start_timing
+    reps += 1
 
-    if not start_timing: #ie: If the timer isn't already started.
-        start_timing = True
-        count_down(WORK_MIN * 60)
+    if reps % 2 != 0 and reps < 8:
+        #Is 1st, 3rd, 5th or 7th rep.
+        print("Work Time")
+        main_title.config(text="Work", fg=GREEN)
+        count_down(WORK_MIN * 60) # Work Time
+
+    elif reps % 2 == 0 and reps < 8:
+        #Is 2nd, 4th or 6th rep.
+        print("Short Break")
+        main_title.config(text="Break", fg=PINK)
+        count_down(SHORT_BREAK_MIN * 60) # Short Break
+
+    elif reps == 8:
+        print("Long Break")
+        main_title.config(text="Break", fg=RED)
+        count_down(LONG_BREAK_MIN * 60) # Long Break
+        reps = 0
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
-    global start_timing
+    global reps
     count_minutes = math.floor(count / 60)
     count_seconds = count % 60
 
-    if count >= 0 and start_timing:
-        canvas.itemconfig(timer_text, text=f"{count_minutes}:{count_seconds:02d}")
-        window.after(1000, count_down, count - 1)
+
+    if count > 0:
+        global timer
+        canvas.itemconfig(timer_text, text=f"{count_minutes}:{int(count_seconds):02d}")
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer() #Timer ran out. Start it again.
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for check in range(work_sessions):
+            marks += "✔"
+
+        checkmarks.config(text=marks)
+
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro Timer")
@@ -51,13 +100,13 @@ canvas.grid(column=1,row=1)
 main_title = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME,42,"bold"))
 main_title.grid(column=1,row=0)
 
-button_start = Button(text="Start",highlightthickness=0,command=start_timer)
+button_start = Button(text="Start",highlightthickness=0,command=button_start_pressed)
 button_start.grid(column=0,row=2)
 
 button_reset = Button(text="Reset",highlightthickness=0, command=reset_timer)
 button_reset.grid(column=2,row=2)
 
-checkmarks = Label(text="✔", fg=GREEN, bg=YELLOW, font=(FONT_NAME,18,"bold"))
+checkmarks = Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME,18,"bold"))
 checkmarks.grid(column=1,row=2)
 
 window.mainloop()
